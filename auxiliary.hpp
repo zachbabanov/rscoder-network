56ab3d3ae0522bec4b68e1fe204b19a993fdbcc7
Zach <zach.fadeev@yandex.com>
2024-11-04 03:40:57 +0300

    Update auxiliary.hpp

--- auxiliary.hpp
+++ auxiliary.hpp
@@ -106,7 +106,7 @@ char* composePacket(char* packet, const char &msgType, const char &fileIndex, co
 char* composePacket(char* packet, const char &msgType, const char16_t &packetIndex, const int &bandwidth)
 {
     packet = new char[255 * bandwidth + 4];
-    memset(&packet[0], 1, 255 * bandwidth + 2);
+    memset(&packet[0], 1, 255 * bandwidth + 4);
 
     char packetIndexLow = static_cast<char>(packetIndex);
     char packetIndexHigh = static_cast<char>(packetIndex >> 8);
